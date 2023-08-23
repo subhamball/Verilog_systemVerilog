5a2463a81af6ddf950f12585b6ea8adfc659faca
Subham Ball <123955835+subhamball@users.noreply.github.com>
2023-08-24 01:41:17 +0530

    Add files via upload

---
+++ majority_detector/majority_detector_tb.sv
@@ -0,0 +1,18 @@
+module testbench;
+  reg a,b,c;
+  wire out;
+  //integer i;
+  majority_detector DUT(a,b,c,out);
+  initial begin
+    for(integer i=0;i<8;i++) begin
+     #10; {a,b,c}= i;
+    end
+    #10; $finish;
+  end
+    
+  initial begin
+    $monitor(a,b,c,out);
+     $dumpfile("dump.vcd");
+    $dumpvars(0);
+    end
+endmodule
