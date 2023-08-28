ab428af178382804859750866c96e375c580f9f3
Subham Ball <123955835+subhamball@users.noreply.github.com>
2023-08-29 05:10:42 +0530

    Add files via upload

---
+++ or_gate/or_gate.sv
@@ -0,0 +1,4 @@
+
+module or_gate(input a,b,output y);
+  assign y = a|b;
+endmodule
---
+++ or_gate/or_gate_tb.sv
@@ -0,0 +1,22 @@
+module tb;
+  reg a,b;
+  wire y;
+  integer i;
+  
+  or_gate DUT(a,b,y);
+  
+  initial begin
+    $monitor("a=%0b, b=%0b, y=%0b",a,b,y);
+    $dumpfile("dump.vcd");
+    $dumpvars(0);
+  end
+  
+  initial begin
+    for(i=0;i<4;i++) begin
+      {a,b} = i;
+      #10;
+    end
+    #10; $finish;
+  end
+endmodule
+      
